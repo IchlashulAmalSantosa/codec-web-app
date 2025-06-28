9bf29401da97333c65fdf5f0fa433e2cbc36198a
amalsantosa <120997152+IchlashulAmalSantosa@users.noreply.github.com>
2025-06-28 18:41:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 app.register_blueprint(main)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # pakai port dari Railway
+    port = int(os.environ.get('PORT', 5000))  # port railway
     app.run(debug=False, host='0.0.0.0', port=port)
